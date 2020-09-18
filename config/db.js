35f02100e8a857212fda181fb4079f993eec3d90
sreeragpavithran <46649210+sreeragpavithran@users.noreply.github.com>
2020-09-18 10:07:33 +0530

    Completed Profile API

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ const db = config.get('mongoURI');
 const connectDB = async()=>{
     try{
         await mongoose.connect(db, {
-            useFindAndModify: true,
+            useFindAndModify: false,
             useUnifiedTopology: true,
             useNewUrlParser: true,
             useCreateIndex: true
