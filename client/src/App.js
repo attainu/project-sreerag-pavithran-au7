d28501a7c17765e217f777d2bbcbf029facb06c0
sreeragpavithran <46649210+sreeragpavithran@users.noreply.github.com>
2020-09-21 10:19:56 +0530

    Imported Custom CSS and Images

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import './App.css';
+import Landing from './components/layout/Landing';
+import Navbar from './components/layout/Navbar';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+
 
 const App = ()=> {
   return (
-    <div className="App">
-      <h1>Hello Word_</h1>
-    </div>
+    <BrowserRouter>
+      <Fragment>
+        <Navbar />
+        <Route path="/" exact component={Landing} />
+        <section className="container">
+          <Switch>
+            <Route path="/login" component={Login} />
+            <Route path="/register" component={Register} />
+          </Switch>
+        </section>
+      </Fragment>
+    </BrowserRouter>
   );
 }
 
